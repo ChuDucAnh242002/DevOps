8dc60fe9b5f0bdb49de9cb755c9340ba0f11c4bb
vboxuser <vboxuser@DevOps.myguest.virtualbox.org>
2024-11-08 22:36:52 +0200

    Exercise 4

--- service1/app.py
+++ service1/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import psutil
 import socket
 import requests
 import time
 import asyncio
+import docker
 
 app = Flask(__name__)
 
@@ -30,13 +31,13 @@ async def get_uptime():
     boot_time = time.time() - psutil.boot_time()
     return boot_time
 
-@app.route('/', methods=['GET'])
+@app.route('/request', methods=['GET'])
 async def service_info():
 
-    # service1_ip_address = await get_ip_address()
-    # service1_running_process = await get_running_processes()
-    # service1_disk_space = await get_disk_space()
-    # service1_uptime = await get_uptime()
+    service1_ip_address = await get_ip_address()
+    service1_running_process = await get_running_processes()
+    service1_disk_space = await get_disk_space()
+    service1_uptime = await get_uptime()
 
     service1_data = {
         'IP Address': await get_ip_address(),
@@ -56,7 +57,25 @@ async def service_info():
         'service 2': service2_data
     }
 
+    time.sleep(2)
+
     return jsonify(info)
 
+@app.route('/login')
+async def login():
+    return render_template('login.html')
+
+@app.route('/', methods=['GET', 'POST'])
+async def home():
+    return render_template('home.html')
+
+@app.route('/stop', methods=['GET'])
+async def stop_system():
+    client = docker.from_env()
+    for container in client.containers.list():
+        container.stop()
+
+    return "System shutting down."
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8199)
+    app.run(host='0.0.0.0', port=5000)
